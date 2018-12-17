41f5ddb5e93e466fdea94bfc4cc85dc90ca62480
TroNine-PC <plume142@gmail.com>
2018-12-18 02:46:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tronine_pc/snakegame/SnakeActivity.java
@@ -0,0 +1,42 @@
+package com.example.tronine_pc.snakegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+
+public class SnakeActivity extends Activity {
+
+    SnakeEngine snakeEngine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Display display = getWindowManager().getDefaultDisplay();
+
+        Point size = new Point();
+        display.getSize(size);
+
+        snakeEngine = new SnakeEngine(this, size);
+
+        setContentView(snakeEngine);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        snakeEngine.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        snakeEngine.pause();
+    }
+
+
+}
